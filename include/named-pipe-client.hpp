f1feff11503fcf614c8d3f3e08234c5c6a120d87
Roman Spychała <epienkowski@gmail.com>
2018-08-07 15:38:13 +0200

    Client - fixes and improvements

--- include/named-pipe-client.hpp
+++ include/named-pipe-client.hpp
@@ -64,6 +64,7 @@ public:
 
 	static const size_t MAX_INPUT_MESSAGE_SIZE = 1024 * 1024 * 1024;
 	static const DWORD CONNECT_TIMEOUT = 1000;
+	static const DWORD WRITE_RETRY_TIMEOUT = 50;
 
 	named_pipe_client(const CString& sPipe)
 		:m_sPipe(sPipe), m_fPendingRead(false), m_fPendingWrite(false)
@@ -110,7 +111,7 @@ public:
 			_ATLCATCH(e)
 			{
 				HRESULT hRes = e;
-				cancel_pending_operations();
+				reset();
 
 				DWORD dwWait = ::WaitForSingleObject(m_evStop, CONNECT_TIMEOUT);
 				switch (dwWait)
@@ -214,8 +215,8 @@ private:
 			case WAIT_OBJECT_0 + 1: // evWrite
 			{
 				DWORD dwBytesTransferred;
-				HRESULT hRes = m_pipe.GetOverlappedResult(&m_ovWrite, dwBytesTransferred, true);
 				m_fPendingWrite = false;
+				HRESULT hRes = m_pipe.GetOverlappedResult(&m_ovWrite, dwBytesTransferred, true);
 				reset_event(m_evWrite);
 			}
 
@@ -239,7 +240,17 @@ private:
 						HRESULT hRes = m_pipe.Write(memory_block.GetData(), static_cast<DWORD>(memory_block.GetSize()), &m_ovWrite);
 						if (SUCCEEDED(hRes))
 						{
-							m_fPendingWrite = false;
+							DWORD dwBytesTransferred;
+							hRes = m_pipe.GetOverlappedResult(&m_ovWrite, dwBytesTransferred, true);
+							if (SUCCEEDED(hRes))
+							{
+								m_fPendingWrite = false;
+								reset_event(m_evWrite);
+							}
+							else
+							{
+								AtlThrow(hRes);
+							}
 						}
 						else if (win32_error<ERROR_IO_PENDING>(hRes))
 						{
@@ -254,7 +265,7 @@ private:
 					break;
 
 					case CANNOT_LOCK_BUFFER_LIST:
-						dwTimeout = 50;
+						dwTimeout = WRITE_RETRY_TIMEOUT;
 						fWrite = false;
 						break;
 
@@ -273,56 +284,63 @@ private:
 			}
 		}
 
-		cancel_pending_operations();
+		reset();
 	}
 
-	void cancel_pending_operations()
+	void reset()
 	{
-		if (m_pipe.m_h == nullptr)
+		if (m_pipe.m_h != nullptr)
 		{
-			return;
-		}
+			HANDLE ahWait[2];
+			DWORD dwCnt = 0, dwBytesTransferred, dwWait;
+			HRESULT hRes;
 
-		HANDLE ahWait[2];
-		DWORD dwCnt = 0;
-
-		if (m_fPendingRead)
-		{
-			ahWait[dwCnt++] = m_evRead;
-			m_pipe.CancelIoEx(&m_ovRead);
-		}
-
-		if (m_fPendingWrite)
-		{
-			ahWait[dwCnt++] = m_evWrite;
-			m_pipe.CancelIoEx(&m_ovWrite);
-		}
-
-		DWORD dwWait = ::WaitForMultipleObjects(dwCnt, ahWait, true, INFINITE);
-		ATLASSERT(dwWait >= WAIT_OBJECT_0 && dwWait < (WAIT_OBJECT_0 + dwCnt));
-
-		HRESULT hRes;
-		DWORD dwBytesTransferred;
-
-		if (m_fPendingRead)
-		{
-			hRes = m_pipe.GetOverlappedResult(&m_ovRead, dwBytesTransferred, true);
-			if (abort_succeeded(hRes))
+			if (m_fPendingRead)
 			{
-				m_fPendingRead = false;
+				ahWait[dwCnt++] = m_evRead;
+				hRes = m_pipe.CancelIoEx(&m_ovRead);
+				ATLASSERT(SUCCEEDED(hRes));
 			}
-		}
 
-		if (m_fPendingWrite)
-		{
-			hRes = m_pipe.GetOverlappedResult(&m_ovWrite, dwBytesTransferred, true);
-			if (abort_succeeded(hRes))
+			if (m_fPendingWrite)
 			{
-				m_fPendingWrite = false;
+				ahWait[dwCnt++] = m_evWrite;
+				hRes = m_pipe.CancelIoEx(&m_ovWrite);
+				ATLASSERT(SUCCEEDED(hRes));
 			}
+
+			dwWait = ::WaitForMultipleObjects(dwCnt, ahWait, true, INFINITE);
+			ATLASSERT(dwWait >= WAIT_OBJECT_0 && dwWait < (WAIT_OBJECT_0 + dwCnt));
+
+			if (m_fPendingRead)
+			{
+				hRes = m_pipe.GetOverlappedResult(&m_ovRead, dwBytesTransferred, true);
+				if (abort_succeeded(hRes))
+				{
+					m_fPendingRead = false;
+				}
+			}
+
+			if (m_fPendingWrite)
+			{
+				hRes = m_pipe.GetOverlappedResult(&m_ovWrite, dwBytesTransferred, true);
+				if (abort_succeeded(hRes))
+				{
+					m_fPendingWrite = false;
+				}
+			}
+
+			m_pipe.Close();
+		}
+		else
+		{
+			m_fPendingRead = false;
+			m_fPendingWrite = false;
 		}
 
-		m_pipe.Close();
+		reset_event(m_evRead);
+		reset_event(m_evWrite);
+		reset_event(m_evNewMsg);
 
 		m_inputBuffer.Clear();
 		m_outputBuffers.RemoveAll();
