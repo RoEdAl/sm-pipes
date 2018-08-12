fe6f5aaebe3e86b7bbbdd01a13259c4dbc980bba
Roman Spychała <roed@onet.eu>
2018-08-12 15:11:52 +0200

    Client - read commands from JSON array
    JSON document may be internal (from resources) or external (from specified file).
    
    CMake - project folders (Server, Client).

--- client-app/client-app.cpp
+++ client-app/client-app.cpp
@@ -4,14 +4,58 @@
 
 #include <targetver.h>
 #include <SMPipesConfig.h>
+#include "resource.h"
 
 #include <stdio.h>
 #include <conio.h>
 #include <tchar.h>
 #include <atl-headers.h>
+#include <rapidjson-headers.hpp>
 
 namespace
 {
+    namespace json = rapidjson;
+
+    UINT get_json_from_resource(UINT nIdrJson, LPCSTR* pszScript)
+    {
+        HMODULE hThisModule = ::GetModuleHandle(nullptr);
+
+        HRSRC hSrc = ::FindResource(hThisModule, MAKEINTRESOURCE(nIdrJson), _T("JSON"));
+        ATLASSERT(hSrc != nullptr);
+
+        HGLOBAL hRes = ::LoadResource(hThisModule, hSrc);
+        ATLASSERT(hRes != nullptr);
+
+        DWORD dwSize = ::SizeofResource(hThisModule, hSrc);
+        ATLASSERT(dwSize > 0);
+
+        LPVOID pRes = ::LockResource(hRes);
+        ATLASSERT(pRes != nullptr);
+
+        if(pszScript != nullptr)
+        {
+            *pszScript = reinterpret_cast<LPCSTR>(pRes);
+        }
+        return (dwSize);
+    }
+
+    json::ParseResult parse_json(json::Document& doc, UINT nIdrJson)
+    {
+
+        LPCSTR psz;
+        UINT nLen;
+
+        nLen = get_json_from_resource(nIdrJson, &psz);
+        ATLASSERT(nLen > 0);
+
+        return doc.Parse(psz, nLen);
+    }
+}
+
+namespace
+{
+    namespace json = rapidjson;
+
 #include <named-pipe-client.hpp>
 #include <security-policy.hpp>
 #include <named-pipe-defaults.hpp>
@@ -24,21 +68,19 @@ namespace
 
     typedef named_pipe_client<named_pipes_defaults::BUFFER_SIZE> np_client;
 
-    template<size_t S>
-    void send_url(np_client& client, const char(&url)[S])
+    void send_msg(np_client& client, const json::Value& msg)
     {
-        CStringA json("{\"cmd\":\"url\",\"val\":\"");
-        json.Append(url);
-        json.Append("\"}");
+        json::StringBuffer strm;
+        json::Writer<json::StringBuffer> writer(strm);
+        msg.Accept(writer);
 
-        size_t nLen = json.GetLength();
+        pipe_client_basics::Buffer buf;
+        size_t nBufferSize = strm.GetSize();
+        buf.SetCount(nBufferSize);
+        Checked::memcpy_s(buf.GetData(), nBufferSize, strm.GetString(), nBufferSize);
 
-        CAtlArray<BYTE> buffer;
-        buffer.SetCount(nLen);
-        Checked::memcpy_s(buffer.GetData(), nLen, (LPCSTR)json, nLen);
-
-        _tprintf(_T("client: sending URL=%S\n"), url);
-        client.SendMessage(buffer);
+        _tprintf(_T("client: sending next message size=%zi\n"), buf.GetCount());
+        client.SendMessage(buf);
     }
 
 	class ClientMessages :public pipe_client_basics::INotify
@@ -80,9 +122,60 @@ namespace
 	};
 }
 
-int main()
+int _tmain(int argc, TCHAR* argv[])
 {
 	_tprintf(_T("client: version=%s\n"), _T(SMPIPES_VERSION_STR));
+
+    json::Document doc;
+
+    if(argc < 2)
+    {
+        json::ParseResult res = parse_json(doc, IDR_JSON_COMMANDS);
+
+        if(!res)
+        {
+            _tprintf(_T("client: unable to parse JSON document - offset %u: %S\n"),
+                (unsigned)res.Offset(),
+                GetParseError_En(res.Code()));
+            return 3;
+        }
+    }
+    else
+    {
+        FILE* f;
+        errno_t  res = _tfopen_s(&f, argv[1], _T("rb"));
+
+        if(res)
+        {
+            _tprintf(_T("client: cannot open file \"%s\" - error code: %d\n"), argv[1], res);
+            return 2;
+        }
+
+        {
+            CAtlArray<char> rbuffer;
+            rbuffer.SetCount(32 * 1024);
+
+            json::FileReadStream fs(f, rbuffer.GetData(), rbuffer.GetCount());
+            json::ParseResult res = doc.ParseStream(fs);
+
+            if(!res)
+            {
+                fclose(f);
+                _tprintf(_T("client: unable to parse JSON document - offset %u: %S\n"),
+                    (unsigned)res.Offset(),
+                    GetParseError_En(res.Code()));
+                return 3;
+            }
+        }
+        fclose(f);
+    }
+
+    if(!doc.IsArray())
+    {
+        _tprintf(_T("client: specified JSON document is not an array.\n"));
+        return 4;
+    }
+
 #ifdef USE_LOGON_SESSION
     default_security_policy security_policy;
     if(!security_policy.Init()) return 1;
@@ -94,23 +187,44 @@ int main()
 	ClientMessages messages;
 	np_client client(sPipeName, messages);
 	messages.SetClient(client);
-	_tprintf(_T("client: pipe=%s\n"), (LPCTSTR)client.GetPipeName());
+	_tprintf(_T("client: opening pipe=%s\n"), (LPCTSTR)client.GetPipeName());
 
 	HRESULT hRes = client.Run();
     _tprintf(_T("client: run %08x\n"), hRes);
 
-	// 1
-    send_url(client, "http://poznan.infometeo.pl");
-	_gettch();
+    if(FAILED(hRes))
+    {
+        return hRes;
+    }
 
-	// 2
-    send_url(client, "http://www.super-memory.com");
-	_gettch();
+    for(auto& v : doc.GetArray())
+    {
+        if(!v.IsObject())
+        {
+            _tprintf(_T("client: skipping non-object array element\n"));
+            continue;
+        }
+
+        if(!(v.HasMember("cmd") && v.HasMember("val")))
+        {
+            _tprintf(_T("client: skipping invalid object array element\n"));
+            continue;
+        }
+
+        send_msg(client, v);
+
+        TCHAR c = _gettch();
+        if(c == _T('q'))
+        {
+            _tprintf(_T("client: exit from loop\n"));
+            break;
+        }
+    }
 
 	_tprintf(_T("client: stop\n"));
-	client.Stop();
+	hRes = client.Stop();
 
 	_gettch();
-	_tprintf(_T("client: bye\n"));
-    return 0;
+	_tprintf(_T("client: bye - %08x\n"), hRes);
+    return hRes;
 }
---
+++ client-app/resource.h
@@ -0,0 +1,10 @@
+/*
+    resource.h
+*/
+
+#ifndef _RESOURCE_H_
+#define _RESOURCE_H_
+
+#define IDR_JSON_COMMANDS 100
+
+#endif // _RESOURCE_H_
