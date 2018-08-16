0f6f74ea47f9e64254adcfad8c25e8c76d8e8f73
Roman Spychała <roed@onet.eu>
2018-08-16 16:17:35 +0200

    Trivial Google Chrome extension.
    Based on Mozilla plugin - minor changes.

---
+++ extensions/google-chrome/background.js
@@ -0,0 +1,38 @@
+//
+// background.js
+//
+
+let
+
+message_handler = (response) => {
+  console.log("sm-pipes: Received: " + JSON.stringify(response));
+},
+
+disconnect_handler = () => {
+  console.log("sm-pipes: Disconnected");
+},
+
+connect_native = function (name, on_message, on_disconnect) {
+    let port = chrome.runtime.connectNative(name);
+    port.onMessage.addListener(on_message);
+    port.onDisconnect.addListener(on_disconnect);
+    return port;
+},
+
+port = connect_native("com.supermemory.nativemsgproxy", message_handler, disconnect_handler),
+
+send_msg = (cmd, val) => {
+    let msg = {
+        "cmd": cmd,
+        "val": val
+    };
+    console.log("sm-pipes: Sending: " + JSON.stringify(msg));
+    port.postMessage(msg);
+};
+
+/*
+    On a click on the browser send URL of current tab
+*/
+chrome.browserAction.onClicked.addListener((tab) => {
+    send_msg("url", tab.url);
+});
