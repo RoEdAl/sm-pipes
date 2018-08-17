1f466f3c438de6dfe2630d5a2b6729a66c0430fa
Roman Spychała <roed@onet.eu>
2018-08-17 21:59:17 +0200

    extensions - handling connect/disconnect messages

--- extensions/google-chrome/background.js
+++ extensions/google-chrome/background.js
@@ -2,10 +2,46 @@
 // background.js
 //
 
+send_to_sm = "Send to SuperMemo",
+cannot_send_to_sm = "Trying to connect to SuperMemo…",
+
+enabled_icon_set = {
+    16: "icons/sm-16.png",
+    32: "icons/sm-32.png",
+    48: "icons/sm-48.png",
+    64: "icons/sm-64.png"
+},
+disabled_icon_set = {
+    16: "icons/disabled/sm-16.png",
+    32: "icons/disabled/sm-32.png",
+    48: "icons/disabled/sm-48.png",
+    64: "icons/disabled/sm-64.png"
+};
+
 let
 
+update_active_tab = (connected) => {
+    if (connected) {
+        chrome.browserAction.enable();
+        chrome.browserAction.setIcon({path: enabled_icon_set});
+        chrome.browserAction.setTitle({title: send_to_sm});
+    } else {
+        chrome.browserAction.disable();
+        chrome.browserAction.setIcon({path: disabled_icon_set});
+        chrome.browserAction.setTitle({title: cannot_send_to_sm});
+    }
+},
+
 message_handler = (response) => {
-  console.log("sm-pipes < " + JSON.stringify(response));
+    console.log("sm-pipes < " + JSON.stringify(response));
+
+    if  (response.hasOwnProperty( 'ntrnl' )) {
+        if (response.ntrnl === 'connected') {
+            update_active_tab(true);
+        } else if (response.ntrnl === 'disconnected') {
+            update_active_tab(false);
+        }
+    }
 },
 
 disconnect_handler = () => {
@@ -19,7 +55,11 @@ connect_native = function (name, on_message, on_disconnect) {
     return port;
 },
 
-port = connect_native("com.supermemory.nativemsgproxy", message_handler, disconnect_handler),
+port = connect_native(
+    "com.supermemory.nativemsgproxy",
+    message_handler,
+    disconnect_handler
+),
 
 send_msg = (cmd, val) => {
     let msg = {
--- extensions/mozilla/background.js
+++ extensions/mozilla/background.js
@@ -2,14 +2,52 @@
 // background.js
 //
 
+const
+
+send_to_sm = "Send to SuperMemo",
+cannot_send_to_sm = "Trying to connect to SuperMemo…",
+
+enabled_icon_set = {
+    16: "icons/sm-16.png",
+    32: "icons/sm-32.png",
+    48: "icons/sm-48.png",
+    64: "icons/sm-64.png"
+},
+disabled_icon_set = {
+    16: "icons/disabled/sm-16.png",
+    32: "icons/disabled/sm-32.png",
+    48: "icons/disabled/sm-48.png",
+    64: "icons/disabled/sm-64.png"
+};
+
 let
 
+update_active_tab = (connected) => {
+    if (connected) {
+        browser.browserAction.enable();
+        browser.browserAction.setIcon({path: enabled_icon_set});
+        browser.browserAction.setTitle({title: send_to_sm});
+    } else {
+        browser.browserAction.disable();
+        browser.browserAction.setIcon({path: disabled_icon_set});
+        browser.browserAction.setTitle({title: cannot_send_to_sm});
+    }
+},
+
 message_handler = (response) => {
-  console.log("sm-pipes < " + JSON.stringify(response));
+    console.log("sm-pipes < " + JSON.stringify(response));
+
+    if (response.hasOwnProperty( 'ntrnl' )) {
+        if (response.ntrnl === 'connected') {
+            update_active_tab(true);
+        } else if (response.ntrnl === 'disconnected') {
+            update_active_tab(false);
+        }
+    }
 },
 
 disconnect_handler = () => {
-  console.log("sm-pipes : disconnected");
+    console.log("sm-pipes : disconnected");
 },
 
 connect_native = function (name, on_message, on_disconnect) {
@@ -19,13 +57,14 @@ connect_native = function (name, on_message, on_disconnect) {
     return port;
 },
 
-port = connect_native("com.supermemory.nativemsgproxy", message_handler, disconnect_handler),
+port = connect_native(
+    "com.supermemory.nativemsgproxy",
+    message_handler,
+    disconnect_handler
+),
 
 send_msg = (cmd, val) => {
-    let msg = {
-        "cmd": cmd,
-        "val": val
-    };
+    let msg = {"cmd": cmd, "val": val};
     console.log("sm-pipes > " + JSON.stringify(msg));
     port.postMessage(msg);
 };
@@ -33,8 +72,6 @@ send_msg = (cmd, val) => {
 /*
     On a click on the browser send URL of current tab
 */
-browser.browserAction.onClicked.addListener(() => {
-  browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
-      send_msg("url", tabs[0].url);
-  });
+browser.browserAction.onClicked.addListener((tab) => {
+    send_msg("url", tab.url);
 });
